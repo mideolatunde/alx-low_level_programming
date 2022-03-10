c97b483c76f9748c910879ac979ba3f22ddfda2a
mideolatunde <ayomideeli2002@gmail.com>
2022-03-10 13:13:20 -0800

    hopefully

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the string without using prinf or put function
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	system( "echo and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
