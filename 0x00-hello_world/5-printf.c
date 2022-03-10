b5e701c544cae7e73154294d591ff0f73d9745d9
mideolatunde <ayomideeli2002@gmail.com>
2022-03-10 11:53:45 -0800

    gsyyd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
- 
- /**
- * main - main block
- * Return: 0
+
+/**
+ * main - testing printf function
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,
-");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
