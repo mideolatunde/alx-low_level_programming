e82afacd6764b6880c41f58d17595e60086db74f
mideolatunde <ayomideeli2002@gmail.com>
2022-03-11 10:09:16 -0800

    gsgs

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,28 +5,28 @@
  * main - prints and executes if the last dgit is
  * greater or equal or less than
  *
- * Return: Always o (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, Lastdigit;
+	int n, m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	Lastdigit = n % 10;
+	m = n % 10;
 
-	if (Lastdigit > 5)
+	if (m > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, Lastdigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
-	else if (Lastdigit < 6 && Lastdigit != 0)
+	else if (m < 6 && m != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n", n, Lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, m);
 	}
 	return (0);
 }
