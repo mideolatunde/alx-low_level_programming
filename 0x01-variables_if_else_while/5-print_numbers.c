6dbb71a0036087d6527ec5d62e4aa0072ec40c80
mideolatunde <ayomideeli2002@gmail.com>
2022-03-11 12:08:19 -0800

    hdhdey

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers from 1 to 10 followed 
+ * by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf(c);
+	}
+	printf('\n');
+	return (0);
+}
