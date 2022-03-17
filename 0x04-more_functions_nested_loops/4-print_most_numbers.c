e085bef16566666a7b7be88e99446553803f4440
mideolatunde <ayomideeli2002@gmail.com>
2022-03-17 16:07:50 -0700

    ffth

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,15 +10,13 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 && i != 4)
-		{
+		if (i == 2 || i == 4)
+			continue;
+		else
 			_putchar(i + '0');
-		}
-		i++;
 	}
 	_putchar('\n');
-	return (0);
 }
 
