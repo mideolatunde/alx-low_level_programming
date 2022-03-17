a1c37dc24615abf7b6a92ec1d19b41beade80fcb
mideolatunde <ayomideeli2002@gmail.com>
2022-03-17 15:47:51 -0700

    ftyh

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,18 +8,17 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	while (i <= 9)
 	{
-		if (i = 2 || i = 4)
-		{
-			continue;
-		}
-		else
+		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 		}
+		i++;
 	}
 	_putchar('\n');
+	return (0);
 }
+
